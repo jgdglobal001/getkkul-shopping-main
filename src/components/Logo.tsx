ea6f9d1ec4d5b96d4290def062b1ebd5d47eec67
jgdglobal001 <jgdglobal001@gmail.com>
2025-10-23 18:01:24 +0900

    fix: reduce logo size to 40% (w-52 to w-32)

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -11,7 +11,7 @@ const Logo = () => {
         alt="겟꿀쇼핑 로고"
         width={210}
         height={84}
-        className="w-52 h-auto"
+        className="w-32 h-auto"
         style={{ width: 'auto', height: 'auto' }}
         unoptimized
       />
