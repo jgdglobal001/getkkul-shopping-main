dc838c5b7184f832ced47c2a9c423f936000740c
jgdglobal001 <jgdglobal001@gmail.com>
2026-01-08 16:27:11 +0900

    (로그인 페이지)은 로봇이 읽어갈 수 있도록 허용

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
         rules: [
             {
                 userAgent: '*',
-                allow: '/',
+                allow: ['/', '/auth/signin'],
                 disallow: [
                     '/admin/*',
                     '/dashboard/*',
