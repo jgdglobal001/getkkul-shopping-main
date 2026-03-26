9278811fa1f7881c4f7afe1842928a23c1dd3466
jgdglobal001 <jgdglobal001@gmail.com>
2026-03-26 12:44:58 +0900

    코드상 가장 유력한 문제점은 방금 수정한 callback route placement 였습니다.

--- src/app/(brandpay-callback)/account/payment/callback/page.tsx
+++ src/app/(brandpay-callback)/account/payment/callback/page.tsx
@@ -1,6 +1,7 @@
-export const runtime = 'edge';
+export const runtime = "edge";
+
 import PaymentCallbackClient from "@/components/account/PaymentCallbackClient";
 
 export default function PaymentCallbackPage() {
-    return <PaymentCallbackClient />;
+  return <PaymentCallbackClient />;
 }
